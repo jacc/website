787ed7b0b37171d30d35209a89ac3bbce6e1a066
Jack LaFond <jack@lafond.id>
2025-05-12 12:40:01 -0400

    feat: click sound

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "@/styles/globals.css";
 import { Lenis } from "lenis/react";
+import { AnimatePresence } from "motion/react";
 import type { AppProps } from "next/app";
 import { Geist, Libre_Baskerville } from "next/font/google";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const body = Geist({
   subsets: ["latin"],
@@ -14,6 +17,18 @@ const serif = Libre_Baskerville({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const click = new Audio("/click.mp3");
+    click.volume = 0.5;
+    void click.play().catch(() => null);
+  }, [router.pathname]);
+
   return (
     <>
       <style jsx global>
@@ -31,7 +46,9 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <Lenis root>
-        <Component {...pageProps} />;
+        <AnimatePresence mode="popLayout">
+          <Component {...pageProps} />;
+        </AnimatePresence>
       </Lenis>
     </>
   );
