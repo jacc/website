80605a837b08ca68a599d407ffa5acb5d3679066
Jack LaFond <jack@lafond.id>
2025-05-24 13:08:11 -0400

    cleanup: layout / unused API file

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -14,7 +14,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({
   showBackButton,
 }) => {
   return (
-    <main className="relative mx-auto max-w-xl px-3 pt-10 md:pt-18 pb-16">
+    <main className="relative mx-auto max-w-xl px-3 pt-10 md:pt-14 pb-16">
       {showBackButton && <BackButton />}
       <motion.div
         className="flex flex-col gap-4"
