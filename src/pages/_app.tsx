9299a217fc14420caa2eb029c3228fd656844613
Jack LaFond <jack@lafond.id>
2025-05-26 13:37:28 -0400

    feat: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,7 +51,21 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <AnimatePresence mode="popLayout">
